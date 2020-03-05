f9e5f2b4976054bae2addfad24c74230ee4e100f
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2020-03-05 17:42:15 +0100

    issue #1 restructuring and creation of gradle build files

--- src/cairo-sip/src/main/java/org/speechforge/cairo/sip/SipResource.java
+++
@@ -1,8 +0,0 @@
-package org.speechforge.cairo.sip;
-
-import java.rmi.RemoteException;
-
-public interface SipResource {
-    public void bye(String sessionId) throws  RemoteException, InterruptedException;
-    public SdpMessage invite(SdpMessage request, String sessionId) throws ResourceUnavailableException, RemoteException;
-}
