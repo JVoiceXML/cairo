3794ed71693ed671ed3af85cc400ad831327e50a
Dirk Schnelle-Walka <dirk.schnelle-walka@harman.com>
2020-03-05 17:42:15 +0100

    issue #1 restructuring and creation of gradle build files

--- src/cairo-client/bin/demo-speechsynth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PACKAGE=org.speechforge.cairo.client.demo.tts
-CLASS=SpeechSynthClient
-
-LOCAL_RTP_PORT=42046
-PROMPT_TEXT="Congratulations! You have successfully installed the Cairo speech server. Please try the other demos to test out Cairo's speech recognition capabilities."
-
-sh launch.sh $PACKAGE.$CLASS $LOCAL_RTP_PORT "$PROMPT_TEXT"
